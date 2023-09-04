5e14f48561a220a6845ed565ebaae960247ad2f9
blondeman <domo23000@gmail.com>
2023-09-04 01:43:45 -0600

    edge splitting and node deletion

--- ShipbuildingDesertRPG/scripts/test/ShipBuilder.gd
+++ ShipbuildingDesertRPG/scripts/test/ShipBuilder.gd
@@ -7,15 +7,13 @@ var shipEdgeScene = preload("res://scenes/test/ship_edge.tscn")
 var selectedNode: ShipNode = null
 
 var shipNodes: Array = Array() #[front, end, node1 top, node1 bot, node2 top, node2 bot, ...]
-var shipEdges: Array = Array()
+var shipEdges: Array = Array() #[edgeA, edgeB, ...]
 
 func _ready():
 	AddFrontBackNodes()
 	AddNode(Vector2(400, GetMiddle()+100))
 	AddEdge(shipNodes[0], shipNodes[2])
-	AddEdge(shipNodes[0], shipNodes[3])
-	AddEdge(shipNodes[1], shipNodes[2])
-	AddEdge(shipNodes[1], shipNodes[3])
+	AddEdge(shipNodes[2], shipNodes[1])
 	
 func AddFrontBackNodes():
 	var front = shipNodeScene.instantiate()
@@ -41,60 +39,94 @@ func _process(delta):
 			UpdateNodeEdgesSimple(selectedNode)
 		else:
 			selectedNode.position = ClampNode(get_global_mouse_position())
-			GetOppositeNode(selectedNode).position = GetOpposite(ClampNode(get_global_mouse_position()))
+			GetOppositeNode(selectedNode).position = GetOppositePoint(ClampNode(get_global_mouse_position()))
 			UpdateNodeEdgesSimple(selectedNode)
 			UpdateNodeEdgesSimple(GetOppositeNode(selectedNode))
 
 #EDGE FUNCTIONS
 
 func AddEdge(nodeA: ShipNode, nodeB: ShipNode):
-	var edge = shipEdgeScene.instantiate()
-	edge.nodeA = nodeA
-	edge.nodeB = nodeB
-	edge.Update()
-	add_child(edge)
-	shipEdges.append(edge)
+	var edgeA = shipEdgeScene.instantiate()
+	edgeA.nodeA = nodeA
+	edgeA.nodeB = nodeB
+	edgeA.shipBuilder = self
+	edgeA.UpdateCollision()
+	add_child(edgeA)
+	shipEdges.append(edgeA)
+	
+	var edgeB = shipEdgeScene.instantiate()
+	edgeB.nodeA = GetOppositeNode(nodeA)
+	edgeB.nodeB = GetOppositeNode(nodeB)
+	edgeB.shipBuilder = self
+	edgeB.UpdateCollision()
+	add_child(edgeB)
+	shipEdges.append(edgeB)
 
 func UpdateNodeEdges(node: ShipNode):
 	for edge in shipEdges:
 		if edge.nodeA == node || edge.nodeB == node:
-			edge.Update()
+			edge.UpdateCollision()
 
 func UpdateNodeEdgesSimple(node: ShipNode):
 	for edge in shipEdges:
 		if edge.nodeA == node || edge.nodeB == node:
-			edge.UpdateSimple()
+			edge.Update()
 
-#func DeleteEdge(edge: ShipEdge):
-#	shipEdges.erase(edge.opposite)
-#	shipEdges.erase(edge)
+func SplitEdge(edge: ShipEdge):
+	var node: ShipNode = AddNode(edge.GetMiddle())
+	AddEdge(edge.nodeA, node)
+	AddEdge(node, edge.nodeB)
+	DeleteEdge(edge)
+	
+	selectedNode = node
+
+func DeleteEdge(edge: ShipEdge):
+	var oppositeEdge = GetOppositeEdge(edge)
+	shipEdges.erase(edge)
+	edge.queue_free()
+	shipEdges.erase(oppositeEdge)
+	oppositeEdge.queue_free()
+
+func GetOppositeEdge(edge: ShipEdge) -> ShipEdge:
+	var pos: int = shipEdges.find(edge)
+	if(pos%2==0):
+		return shipEdges[pos+1]
+	else:
+		return shipEdges[pos-1]
 
 #NODE FUNCTIONS
-
-#func CreateNode():
-#	var new_node = selectedMenuItem.node.instantiate()
-#	new_node.position = get_global_mouse_position()
-#	add_child(new_node)
-#	shipNodes.append(new_node)
-#
-#	var new_node_opposite = selectedMenuItem.node.instantiate()
-#	new_node_opposite.position = GetOppositePoint(get_global_mouse_position())
-#	add_child(new_node_opposite)
-#	shipNodes.append(new_node_opposite)
-#
-#	new_node.opposite = new_node_opposite
-#	new_node_opposite.opposite = new_node
-#
-#	#selectedMenuItem = null
 
 func SelectNode(node: ShipNode):
 	selectedNode = node
 
 func DeleteNode(node: ShipNode):
-#	for edge in shipEdges:
-#		if edge.nodeA == node || edge.nodeB == node:
-#			edge.Delete()
+	if node.isMiddle: return
+	if shipNodes.size()<=4: return
+	var nodeA = null
+	var edgeA = null
+	var nodeB = null
+	var edgeB = null
+	
+	for edge in shipEdges:
+		if edge.nodeB == node:
+			edgeA = edge
+			edgeB = GetOppositeEdge(edge)
+		if edge.nodeA == node:
+			nodeA = edge.nodeB
+			nodeB = GetOppositeEdge(edge).nodeB
+			DeleteEdge(edge)
+	
+	edgeA.nodeB = nodeA
+	edgeB.nodeB = nodeB
+	
+	var oppositeNode = GetOppositeNode(node)
 	shipNodes.erase(node)
+	node.queue_free()
+	shipNodes.erase(oppositeNode)
+	oppositeNode.queue_free()
+	
+	edgeA.UpdateCollision()
+	edgeB.UpdateCollision()
 
 func ReleaseNode():
 	if(selectedNode.isMiddle):
@@ -103,19 +135,19 @@ func ReleaseNode():
 	else:
 		selectedNode.position = ClampNode(selectedNode.position)
 		UpdateNodeEdges(selectedNode)
-		GetOppositeNode(selectedNode).position = ClampNode(GetOpposite(selectedNode.position))
+		GetOppositeNode(selectedNode).position = ClampNode(GetOppositePoint(selectedNode.position))
 		UpdateNodeEdges(GetOppositeNode(selectedNode))
 		
 	selectedNode = null
 
-func AddNode(position: Vector2):
+func AddNode(position: Vector2) -> ShipNode:
 	var nodeA = shipNodeScene.instantiate()
 	nodeA.position = position
 	nodeA.shipBuilder = self
 	add_child(nodeA)
 	
 	var nodeB = shipNodeScene.instantiate()
-	nodeB.position = GetOpposite(position)
+	nodeB.position = GetOppositePoint(position)
 	nodeB.shipBuilder = self
 	add_child(nodeB)
 	
@@ -126,9 +158,7 @@ func AddNode(position: Vector2):
 		shipNodes.append(nodeB)
 		shipNodes.append(nodeA)
 		
-	print(nodeB.position)
-	for i in shipNodes:
-		print(i.position)
+	return nodeA
 
 func ClampNode(pos: Vector2) -> Vector2:
 	var x = $CollisionShape2D.position.x
@@ -147,15 +177,13 @@ func ClampNodeMiddle(pos: Vector2) -> Vector2:
 func GetMiddle() -> int:
 	return $CollisionShape2D.position.y
 
-func GetOpposite(position: Vector2) -> Vector2:
+func GetOppositePoint(position: Vector2) -> Vector2:
 	return Vector2(position.x, GetMiddle() - position.y + GetMiddle())
 
 func GetOppositeNode(node: ShipNode) -> ShipNode:
+	if(node.isMiddle): return node
 	var pos: int = shipNodes.find(node)
 	if(pos%2==0):
 		return shipNodes[pos+1]
 	else:
 		return shipNodes[pos-1]
-
-#func GetOppositePoint(pos: Vector2) -> Vector2:
-#	return Vector2(pos.x, $CollisionShape2D.position.y + ($CollisionShape2D.position.y - pos.y))
--- ShipbuildingDesertRPG/scripts/test/ShipEdge.gd
+++ ShipbuildingDesertRPG/scripts/test/ShipEdge.gd
@@ -1,25 +1,33 @@
 extends Line2D
 class_name ShipEdge
 
+var shipBuilder: ShipBuilder = null
 var nodeA: ShipNode = null
 var nodeB: ShipNode = null
 
 func Update():
 	if nodeA != null && nodeB != null:
 		points = [nodeA.position, nodeB.position]
-		UpdateCollision()
-
-func UpdateSimple():
-	if nodeA != null && nodeB != null:
-		points = [nodeA.position, nodeB.position]
+		$handle.position = GetMiddle()
 
 func UpdateCollision():
+	Update()
+	SetCollision()
+
+func SetCollision():
 	for i in points.size() - 1:
 		var new_shape = CollisionShape2D.new()
-		$Area2D.add_child(new_shape)
+		$edge_collision.add_child(new_shape)
 		var rect = RectangleShape2D.new()
 		new_shape.position = (points[i] + points[i + 1]) / 2
 		new_shape.rotation = points[i].direction_to(points[i + 1]).angle()
 		var length = points[i].distance_to(points[i + 1])
 		rect.extents = Vector2(length / 2, 10)
 		new_shape.shape = rect
+
+func GetMiddle() -> Vector2:
+	return (nodeA.position + nodeB.position) / 2
+
+func _on_handle_input_event(viewport, event, shape_idx):
+	if event.is_action_pressed("ship_drag"):
+		shipBuilder.SplitEdge(self)
